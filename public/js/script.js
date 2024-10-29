1fd8a86ca0afbba0ee07d46d7a9c426271bf418c
dunewalkin <sergei.gunchenko@gmail.com>
2024-10-30 00:21:51 +0400

    again and again

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const links = document.querySelectorAll('.sidebar a');
+   const contentDiv = document.getElementById('content');
+
+   const setActiveLink = (activeLink) => {
+      links.forEach(link => link.classList.remove('link-active'));
+      activeLink.classList.add('link-active');
+   };
+
+   const loadPage = (page, updateHistory = true) => {
+      fetch(`/views/${page}.html`)
+         .then(response => response.text())
+         .then(data => {
+            contentDiv.innerHTML = data;
+            if (updateHistory) {
+               window.history.pushState({page: page}, "", page === 'about' ? '/' : `#${page}`);
+            }
+         })
+         .catch(error => console.error('Ошибка загрузки:', error));
+   };
+
+   links.forEach(link => {
+      link.addEventListener('click', (event) => {
+         event.preventDefault();
+         const page = event.target.getAttribute('data-page');
+         loadPage(page);
+         setActiveLink(event.target);
+      });
+   });
+
+   // Загрузка начальной страницы
+   const initialPage = window.location.hash ? window.location.hash.slice(1) : 'about';
+   loadPage(initialPage, initialPage !== 'about');
+   setActiveLink(document.querySelector(`.sidebar a[data-page="${initialPage}"]`));
+
+   // Обработка события popstate для навигации браузера
+   window.addEventListener('popstate', (event) => {
+      const page = event.state ? event.state.page : 'about';
+      loadPage(page, false); // загружаем страницу без добавления в историю
+      setActiveLink(document.querySelector(`.sidebar a[data-page="${page}"]`));
+   });
+});
+
