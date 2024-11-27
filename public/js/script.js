ba4bad6076ca5f683fbdd9b7a57dbf17fda71f53
dunewalkin <sergei.gunchenko@gmail.com>
2024-11-27 20:01:26 +0400

    fixing heights of bandcamp frames

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
          .then(response => response.text())
          .then(data => {
             contentDiv.innerHTML = data;
+
+            updateAllHeights();
+
             if (updateHistory) {
                window.history.pushState({page: page}, "", page === 'about' ? '/' : `#${page}`);
             }
@@ -38,4 +41,54 @@ document.addEventListener('DOMContentLoaded', () => {
       setActiveLink(document.querySelector(`.sidebar a[data-page="${page}"]`));
    });
 });
+
+function updateAllHeights() {
+   const boxes = document.querySelectorAll('.bandcamp-item iframe');
+   const maxWidth1 = 989; 
+   const maxWidth2 = 899;
+   const maxWidth3 = 768;
+   const maxWidth4 = 659;
+   const maxWidth5 = 500;
+   const maxWidth6 = 331;
+   const baseHeight = 470; 
+   const thirdHeight = 440;
+   const secondaryHeight = 520; 
+   const baseWidth = 350; 
+
+   boxes.forEach(box => {
+      let height = baseHeight; 
+
+
+      if (window.innerWidth <= maxWidth6) {
+         const extraHeight = 0.6 * (maxWidth6 - window.innerWidth); 
+         height = secondaryHeight + extraHeight; 
+      } else if (window.innerWidth <= maxWidth5) {
+         const extraHeight = 0.29 * (maxWidth5 - window.innerWidth); 
+         height = thirdHeight + extraHeight; 
+      } else if (window.innerWidth <= maxWidth4) {
+         const extraHeight = 0.4 * (maxWidth4 - window.innerWidth); 
+         height = secondaryHeight + extraHeight; 
+      } else if (window.innerWidth <= maxWidth3) {
+         const extraHeight = 0.2 * (maxWidth3 - window.innerWidth); 
+         height = baseHeight + extraHeight; 
+      } else if (window.innerWidth <= maxWidth2) {
+         const extraHeight = 0.4 * (maxWidth2 - window.innerWidth); 
+         height = secondaryHeight + extraHeight; 
+      } else if (window.innerWidth <= maxWidth1) {
+         
+         const extraHeight = 0.2 * (maxWidth1 - window.innerWidth); 
+         height = baseHeight + extraHeight;
+      }
+
+      box.style.aspectRatio = `${baseWidth} / ${height}`;
+   });
+}
+
+window.addEventListener('load', () => {
+   setTimeout(updateAllHeights, 100); 
+   window.addEventListener('resize', updateAllHeights);
+});
+
+
+
 
